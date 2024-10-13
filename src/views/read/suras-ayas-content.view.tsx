526a7bd7bc0282b056d62fb04dc1f5435f409b6f
Naibe mehari tekle <naibemt@uia.no>
2024-10-13 17:24:28 +0200

    final update .5

--- src/views/read/suras-ayas-content.view.tsx
+++ src/views/read/suras-ayas-content.view.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Card from '../../components/card/card.component';
-import surahData from '../../assets/data/adapted_new_suras.json';
+import surahData from '../../assets/data/suras.json';
 import styles from './styles/surah.module.css';
 
 interface Surah {
